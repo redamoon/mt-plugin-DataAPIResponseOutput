ea07873edec76e4e88868a59daf81f19131e7e9d
redamoon <yuuki60824@gmail.com>
2020-12-23 10:58:52 +0900

    fix

--- plugins/DataAPIResponseOutput/lib/DataAPIResponseOutput/Tags.pm
+++ plugins/DataAPIResponseOutput/lib/DataAPIResponseOutput/Tags.pm
@@ -6,27 +6,17 @@ use MT;
 require HTTP::Request;
 use LWP::UserAgent;
 
-sub doLog {
-    my ($msg, $class) = @_;
-    return unless defined($msg);
-
-    require MT::Log;
-    my $log = new MT::Log;
-    $log->message($msg);
-    $log->level(MT::Log::DEBUG());
-    $log->class($class) if $class;
-    $log->save or die $log->errstr;
-}
-
 sub _hdlr_response_output {
     my ( $ctx, $args, $cond ) = @_;
-    my $blog = $ctx->stash( 'blog' );
+    my $site_id = $args->{ id } || 1;  # 初期値 blog_id 1
+    my $blog = MT::Blog->load($site_id);
     my $cgi = MT->config->AdminCGIPath || MT->config->CGIPath;
-    my $url = $args->{ url } || $blog->site_url;
-    my $dataapi = $args->{ dataapi } || $cgi . 'mt-data-api.cgi/';
-    my $endpoint = $args->{ endpoint } || '/sites';
-    my $version = $args->{ version } || 'v4';
-    my $param = $args->{ param };
+    my $url = $args->{ url } || $blog->site_url;  # url 初期値 MTインストールURL
+    $url = $1 if($url=~/(.*)\/$/);  # 末尾の / スラッシュ削除
+    my $dataapi = $args->{ dataapi } || $cgi . 'mt-data-api.cgi/';  # dataapi 初期値 MTインストールパス + mt-data-api.cgi
+    my $endpoint = $args->{ endpoint } || '/sites';  # endpoint 初期値 /sites
+    my $version = $args->{ version } || 'v4';  # version 初期値 v4
+    my $param = $args->{ param };  # param 初期値 なし
     my $ua = LWP::UserAgent->new;
     my $request = HTTP::Request->new(GET => $url . $dataapi . $version . $endpoint . $param);
     my $response = $ua->request($request);
